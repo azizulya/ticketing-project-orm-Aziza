29ec575e1ca370f9d0c1ade96fede8e91beabb80
Aziza Rakhmonova <arakhmonova@foundationmedicine.com>
2022-05-10 20:44:02 -0400

    added roles and users

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,6 +1,12 @@
 package com.cydeo.service;
 
 import com.cydeo.dto.RoleDTO;
+import org.springframework.stereotype.Service;
 
-public interface RoleService extends CrudService<RoleDTO, Long> {
+import java.util.List;
+
+@Service
+public interface RoleService {
+    List<RoleDTO> listAllRoles();
+    RoleDTO findById(Long id);
 }
